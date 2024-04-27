d83a1a0d1dd1fa6b04634e13a7b1fd61407fe7e2
alexnicacio <alexnicacio88@gmail.com>
2024-04-27 18:59:59 -0300

    aula 2 curso JS CoderHouse.

---
+++ Aula2/index.js
@@ -0,0 +1,27 @@
+var primeiroNumero = 1;
+var segundoNumero = 3;
+
+var soma = primeiroNumero + segundoNumero;
+console.log("Soma: ",soma);
+
+
+// var nomeInserido = prompt("Insira seu nome: ");
+
+// console.log("Nome Inserido: ", nomeInserido)
+
+// var mensagemAlert = "O nome inserido foi o " + nomeInserido;
+
+// alert(mensagemAlert);
+
+var num1 = prompt("Digite 1 numero")
+var num2 = prompt("Digite o segundo numero")
+
+var num1number = parseInt(num1);
+var num2number = parseInt(num2);
+
+var soma1 = num1number + num2number;
+console.log(soma1);
+
+var produto = num1number * num2number;
+
+alert("A muliplicação é: "+ produto);
